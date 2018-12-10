525ba8ffb9f29fc102cd2f6753f0e1d401391d29
Anton Arefev <qdzxhdsay@mail.ru>
2018-12-10 22:35:28 +0500

    kek

--- hookclientwidget/script.js
+++ hookclientwidget/script.js
@@ -1,26 +1,26 @@
+let headerBlock = document.getElementsByClassName('header-brand')[0];
+
 window.addEventListener('scroll', function() {
-    if(document.documentElement.scrollTop > 10){
-        document.getElementsByClassName('header-brand')[0].classList.add("dropShadow");
+    console.log("D");
+    if(window.pageYOffset>0){
+        headerBlock.classList.add('dropShadow');
     }else{
-        document.getElementsByClassName('header-brand')[0].classList.remove("dropShadow");
+        headerBlock.classList.remove('dropShadow');
     }
 });
 
-
-/*let quickWriteLink = document.getElementsByClassName("card-list-item-quickwrite-date");
-let quickWriteTime = document.getElementsByClassName("card-list-item-quickwrite-time");*/
 
 const masterBlocks = document.querySelectorAll('.master-cards-wrapper');
 
 masterBlocks.forEach(function (block) {
-   let buttons = block.querySelectorAll('button.button.card-list-item-quickwrite-date');
-   buttons.forEach(function (button) {
-       button.addEventListener('click', function(event){
-           let timeline = block.querySelector('.card-list-item-quickwrite-time');
-           if(timeline.classList.contains('qw')) timeline.classList.remove('qw');
-           else timeline.classList.add('qw');
-       })
-   })
+    let buttons = block.querySelectorAll('button.button.card-list-item-quickwrite-date');
+    buttons.forEach(function (button) {
+        button.addEventListener('click', function(event){
+            let timeline = block.querySelector('.card-list-item-quickwrite-time');
+            if(timeline.classList.contains('qw')) timeline.classList.remove('qw');
+            else timeline.classList.add('qw');
+        })
+    })
 });
 
 
@@ -30,37 +30,38 @@ masterBlocks.forEach(function (block) {
 
 
 
-/*for (let i = 0; i < quickWriteLink.length; i++) {
-    let collectLinks = quickWriteLink[i];
-    collectLinks.addEventListener('click', function () {
-        if (collectLinks.classList.contains('quickwrite-active')) {
-            collectLinks.classList.remove("quickwrite-active");
-            let korobka = collectLinks.parentNode.parentNode.parentNode;
+/*
+let headerBlock = document.getElementsByClassName('header-brand')[0];
 
-            for (let j = 0; j < quickWriteTime.length; j++) {
-                let collectTime = quickWriteTime[j];
-                collectTime.classList.remove('qw');
-            }
-        }
-        else {
-            collectLinks.classList.add("quickwrite-active");
+window.onscroll = function() {
+    console.log("huy");
+    if(window.pageYOffset>10){
+       if(headerBlock.classList.contains('dropShadow')){
 
-            let korobka = collectLinks.parentNode.parentNode.parentNode;
+       }
+       else{
+           headerBlock.classList.add('dropShadow');
+       }
+    }
+    else if(window.pageYOffset<10){
+        headerBlock.classList.remove('dropShadow');
+    }
+
+};
+
+*/
 
 
-            for (let j = 0; j < quickWriteTime.length; j++) {
-                let collectTime = quickWriteTime[j];
-                collectTime.classList.add('qw');
-            }
-        }
-    });
-}*/
 
 /*
-quickWriteButtons.addEventListener('click',function () {
-    if(quickWriteButtons.classList.contains('quickwrite-active')){
-        quickWriteButtons.classList.remove("quickwrite-active");
-    }else{
-        quickWriteButtons.classList.add("active");
+window.onscroll = function() {
+    if (document.documentElement.scrollTop >= 10) {
+        document.getElementsByClassName('header-brand')[0].classList.add('dropShadow');
+    } else {
+        document.getElementsByClassName('header-brand')[0].classList.remove('dropShadow');
     }
-});*/
+};
+*/
+
+
+
