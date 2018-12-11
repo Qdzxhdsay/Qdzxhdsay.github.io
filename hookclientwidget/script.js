64fa9c0d030c4072e2eaef2b8185f7836eff56d8
Anton Arefev <qdzxhdsay@mail.ru>
2018-12-11 18:47:05 +0500

    fix

--- hookclientwidget/script.js
+++ hookclientwidget/script.js
@@ -1,7 +1,7 @@
 let headerBlock = document.getElementsByClassName('header-brand')[0];
 
+
 window.addEventListener('scroll', function() {
-    console.log("D");
     if(window.pageYOffset>0){
         headerBlock.classList.add('dropShadow');
     }else{
@@ -24,44 +24,6 @@ masterBlocks.forEach(function (block) {
 });
 
 
-
-
-
-
-
-
-/*
-let headerBlock = document.getElementsByClassName('header-brand')[0];
-
-window.onscroll = function() {
-    console.log("huy");
-    if(window.pageYOffset>10){
-       if(headerBlock.classList.contains('dropShadow')){
-
-       }
-       else{
-           headerBlock.classList.add('dropShadow');
-       }
-    }
-    else if(window.pageYOffset<10){
-        headerBlock.classList.remove('dropShadow');
-    }
-
-};
-
-*/
-
-
-
-/*
-window.onscroll = function() {
-    if (document.documentElement.scrollTop >= 10) {
-        document.getElementsByClassName('header-brand')[0].classList.add('dropShadow');
-    } else {
-        document.getElementsByClassName('header-brand')[0].classList.remove('dropShadow');
-    }
-};
-*/
 
 
 
