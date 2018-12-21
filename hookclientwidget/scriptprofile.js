6eee4e11f7fd0f281bed6a8defe611efefe328e2
Anton Arefev <qdzxhdsay@mail.ru>
2018-12-21 21:01:45 +0500

    add:profile design

--- hookclientwidget/scriptprofile.js
+++ hookclientwidget/scriptprofile.js
@@ -1,10 +1,15 @@
 let profileBar = document.getElementsByClassName('profile-bar')[0];
-
+let profileCard = document.getElementsByClassName('profile-card')[0];
+let profileName = document.getElementsByClassName('profile-bar-name')[0];
 
 window.addEventListener('scroll', function() {
-    if(window.pageYOffset>450){
+    if(window.pageYOffset>370){
         profileBar.classList.add('dropShadow');
+        profileName.classList.add('vs');
+        profileCard.classList.add('vm');
     }else{
         profileBar.classList.remove('dropShadow');
+        profileName.classList.remove('vs');
+        profileCard.classList.remove('vm');
     }
 });
