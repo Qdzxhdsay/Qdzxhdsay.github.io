fcb8e2f12336966759f6329c6440464b6f871ada
Anton Arefev <qdzxhdsay@mail.ru>
2019-02-20 12:50:23 +0500

    new tpl tabs

---
+++ tplhook/script.js
@@ -0,0 +1,38 @@
+var tabContent = document.getElementsByClassName('tab-content');
+var tab = document.getElementsByClassName('tab');
+console.log(tab);
+console.log(tabContent);
+
+
+
+document.getElementById('tabs').onclick = function (event) {
+    var target = event.target;
+
+    if (target.className ==='tab') {
+        for (var i=0; i<tab.length; i++) {
+            if (target === tab[i]) {
+                showTabsContent(i);
+                break;
+            }
+        }
+    }
+};
+
+
+function hideTabsContent(a) {
+    for (var i=a; i<tabContent.length; i++) {
+        tabContent[i].classList.remove('show');
+        tabContent[i].classList.add("hide");
+        tab[i].classList.remove('activeboard');
+    }
+}
+
+
+function showTabsContent(b){
+    if (tabContent[b].classList.contains('hide')) {
+        hideTabsContent(0);
+        tab[b].classList.add('activeboard');
+        tabContent[b].classList.remove('hide');
+        tabContent[b].classList.add('show');
+    }
+}
