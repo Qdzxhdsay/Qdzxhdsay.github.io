0dcce89edc8f296bad0d1a02282d57f99f61c5f4
Qdzxhdsay <qdzxhdsay@mail.ru>
2019-04-12 18:35:28 +0500

    j

---
+++ HOOK - FIRST CLIENT WIDGET/script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let quickPickerBtn = document.getElementById('quickPicker');
+    let headerShadow = document.getElementsByClassName('main-header hipster-widget__header')[0];
+    let scrolledContent = document.getElementsByClassName('hipster-widget__content')[0];
+    
+scrolledContent.addEventListener('scroll', function() {
+    if(scrolledContent.pageYOffset>0||scrolledContent.scrollTop>0){
+        headerShadow.classList.add('dropShadow');
+    }else{
+        headerShadow.classList.remove('dropShadow');
+    }
+});
+
+
+quickPickerBtn.addEventListener('click',function () {
+    let editing = document.querySelector('.quickwrite-bar');
+    if (editing.classList.contains('quickwrite-bar_show')){
+        editing.classList.remove('quickwrite-bar_show');
+    }
+    else{
+        editing.classList.add('quickwrite-bar_show');
+    }
+
+    });
+});
