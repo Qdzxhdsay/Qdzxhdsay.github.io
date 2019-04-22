7400c745f25a2c5bdecbd148aa221bfb311574ff
Qdzxhdsay <qdzxhdsay@mail.ru>
2019-04-22 17:56:13 +0500

    ыва

--- HOOK - FIRST CLIENT WIDGET/script.js
+++ HOOK - FIRST CLIENT WIDGET/script.js
@@ -1,21 +1,7 @@
-/*
-
 let headerShadow;
-let quickPickerBtn;
-let selectBtn;
-let quickPickerPanel;
-let quickPanelItem;
-
-
 
 document.addEventListener('DOMContentLoaded', function () {
     headerShadow = document.getElementsByClassName('main-header hipster-widget__header')[0];
-    quickPickerBtn = document.querySelectorAll('.search-bar__navbtn')[1];
-    selectBtn = document.querySelectorAll('.select-buttons__btnitem');
-    quickPickerPanel = document.getElementsByClassName('quickwrite-bar')[0];
-    quickPanelItem = document.querySelectorAll('.quickwrite-bar__item');
-
-
 
     window.addEventListener('scroll', function() {
     if(window.pageYOffset>0){
@@ -25,54 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
-
-quickPickerBtn.addEventListener('click', function () {
-    if (quickPickerPanel.classList.contains('quickwrite-bar_show')) {
-        quickPickerPanel.classList.remove('quickwrite-bar_show');
-    }
-    else {
-        quickPickerPanel.classList.add('quickwrite-bar_show');
-    }
 });
-
-quickPanelItem.forEach((el) => {
-    el.addEventListener('click', (ev) => {
-        if(el.classList.contains('quickwrite-bar__pickeddate')) {
-            el.classList.remove('quickwrite-bar__pickeddate');
-        }
-        else {
-            el.classList.add('quickwrite-bar__pickeddate');
-        }
-    })
-});
-
-
-selectBtn.forEach((el) => {
-    el.addEventListener('click', (ev) => {
-        if(el.classList.contains('select-buttons_pickedbtn')) {
-            el.classList.remove('buttons_pickedbtn');
-
-        }
-        else {
-            el.classList.add('buttons_pickedbtn');
-        }
-    });
-});
-
-
-    selectBtn.forEach((el) => {
-        el.addEventListener('click', (ev) => {
-            if(el.classList.contains('select-buttons_pickedbtn')) {
-                el.classList.remove('buttons_pickedbtn');
-
-            }
-            else {
-                el.classList.add('buttons_pickedbtn');
-            }
-        });
-    });
-
-});*/
 
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.quick_hide').forEach((el) => {
@@ -104,6 +43,26 @@ document.addEventListener('DOMContentLoaded', () => {
                         break;
                 }
             });
+        });
+    });
+
+    document.querySelectorAll('.class_drop').forEach((el) => {
+        el.addEventListener('click', (ev) => {
+            document.querySelectorAll(`.${el.dataset.drop}`).forEach((del) => {
+                del.classList.remove(el.dataset.classd);
+            });
+        });
+    });
+    document.querySelectorAll('.class_add').forEach((el) => {
+        el.addEventListener('click', (ev) => {
+            document.querySelectorAll(`.${el.dataset.add}`).forEach((ael) => {
+                ael.classList.add(el.dataset.classa);
+            });
+        });
+    });
+    document.querySelectorAll('.class_change').forEach((el) => {
+        el.addEventListener('click', (ev) => {
+            el.classList.toggle(el.dataset.class);
         });
     });
 });
